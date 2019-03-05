891fddef413552d45fac007d9da8461bf06a1283
Macy Graham-Kimbrough <magi196502@yahoo.com>
2019-03-04 20:01:10 -0600

    Setup project skeleton
    Project skeleton setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Landing from "./components/landing";
+import Navbar from "./components/navBar";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Navbar />
         <div className="container">
           <h1>App</h1>
           <Landing />
